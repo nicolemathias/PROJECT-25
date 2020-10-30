6e58b3bf39ad76a07797949af86a4be36dfe2169
nicolemathias <72127523+nicolemathias@users.noreply.github.com>
2020-10-30 13:37:28 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,29 @@
+class paper{
+    constructor(x,y,radius){
+        var options = {
+            isStatic:false,
+            restitution : 0.3,
+            friction: 0.5,
+            density:1.2
+
+        }
+        this.body = Bodies.circle(x,y,radius,options)
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.image = loadImage("sprites/paper.png");
+        World.add(world,this.body)
+    }
+
+    display(){
+        var pos = this.body.position;
+        
+        push();
+        translate(pos.x,pos.y)
+        imageMode (CENTER)
+        image(this.image,0,0,this.radius)
+        ellipse(0,0,this.radius)
+        pop();
+
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,45 @@
+var paperSprite,paperObj;
+var paperBody,ground1;
+
+var dustbin1,dustbin2,dustbin3;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+	
+	engine = Engine.create();
+	world = engine.world;
+
+	dustbin1 = new dustbin(1200,650)
+  
+	paperObj= new paper(200,450,40)
+
+	ground1 = new ground(width/2,670,width,20)
+	
+	var render = Render.create({
+		element: document.body,engine:engine,options:{width:1200,height:700,wireframes: false}})
+	Engine.run(engine);
+}
+
+function draw() {
+  background(0);
+  rectMode(CENTER);
+  
+  paperObj.display();
+  ground1.display();
+  dustbin1.display();
+
+  //Matter.Body.setStatic(helicopterSprite,false);
+}
+
+function keyPressed(){
+ if (keyCode === UP_ARROW){
+
+	Matter.Body.applyForce(paperObj.body,paperObj.body.position,{x:85,y:-85})
+ }};
